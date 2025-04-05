b51ce4e7c79324f1189d3fcda726e97f7fea0ecc
julmx <julmx@sl.erucipe.net>
2025-04-05 11:16:26 +0200

    fix: try for diplay relative position

--- hosts/mainPC/default.nix
+++ hosts/mainPC/default.nix
@@ -54,13 +54,14 @@
   # Dual monitor configuration.
   services.xserver.xrandrHeads = [
    {
+    output = "HDMI-A-0";
+    monitorConfig = ''Option "LeftOf" "DisplayPort-1"'';
+   }
+   
+   {
     output = "DisplayPort-1";
     primary = true;
     monitorConfig = ''Option "RightOf" "HDMI-A-0"'';
-   }
-   {
-    output = "HDMI-A-0";
-    monitorConfig = ''Option "LeftOf" "DisplayPort-1"'';
    }
   ];
 
